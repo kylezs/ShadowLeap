adb821e1ab0dc0bf2c56f8dedda369ad06c75a6e
Kyle Zsembery <zsembery.kyle@gmail.com>
2018-08-26 16:31:38 +1000

    Initial commit, base proj provided w/ libraries linked

---
+++ src/World.java
@@ -0,0 +1,16 @@
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+
+public class World {
+	public World() {
+		// Perform initialisation logic
+	}
+	
+	public void update(Input input, int delta) {
+		// Update all of the sprites in the game
+	}
+	
+	public void render(Graphics g) {
+		// Draw all of the sprites in the game
+	}
+}
