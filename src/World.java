ad68d16d12cb536ca6e8ca462b6f269ba9536b1c
Kyle Zsembery <zsembery.kyle@gmail.com>
2018-08-26 17:16:18 +1000

    World background complete

--- src/World.java
+++ src/World.java
@@ -1,9 +1,20 @@
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
 
 public class World {
-	public World() {
-		// Perform initialisation logic
+  private Image grassTile;
+  private Image waterTile;
+
+  private static final int TILE_SIZE = 48;
+  private static final int START_WATER = 336;
+  private static final int END_WATER = 48;
+	public World() throws SlickException {
+		// set the background tiles
+    grassTile = new Image("assets/grass.png");
+    waterTile = new Image("assets/water.png");
 	}
 	
 	public void update(Input input, int delta) {
@@ -11,6 +22,20 @@ public class World {
 	}
 	
 	public void render(Graphics g) {
-		// Draw all of the sprites in the game
-	}
+    // Draw all of the sprites in the game
+
+    // render the water
+    for (int y_coord = START_WATER; y_coord >= END_WATER; y_coord -= TILE_SIZE) {
+      draw_row(waterTile, y_coord);
+    }
+
+    // render the grass
+    draw_row(grassTile, 672);
+    draw_row(grassTile, 384);
+  }
+
+  private static void draw_row(Image tile, int y_coord) {
+    for (int j = 0; j <= (App.SCREEN_WIDTH / TILE_SIZE); j++)
+      tile.draw(TILE_SIZE * j, y_coord);
+    }
 }
